d05788c758c6328c92487b09a165633b26c4589c
Tibor Helmig <tibor.helmig@gmail.com>
2023-12-12 11:39:15 +0100

    PEP 257 docstring added

--- data_loader.py
+++ data_loader.py
@@ -1,6 +1,20 @@
 import json
 
 def load_required_item_list():
+    """
+    Load the required item list from a JSON file.
+
+    This function reads the required item list from a JSON file located at "data/required.json"
+    and returns the loaded data. The loaded data is expected to contain information about
+    the required items, including their attributes and classes.
+
+    Returns:
+        dict: A dictionary containing information about the required items.
+
+    Raises:
+        FileNotFoundError: If the specified JSON file is not found.
+        json.JSONDecodeError: If there is an issue decoding the JSON data.
+    """
     try:
         with open("data/required.json", "r") as file:
             return json.load(file)
@@ -13,6 +27,20 @@ def load_required_item_list():
 
 
 def load_game_scope_data():
+    """
+    Load game scope data from a JSON file.
+
+    This function reads the game scope data from a JSON file located at "data/game_scope_data.json"
+    and returns the loaded data. The loaded data is expected to contain information about
+    the game's scope, including slots and attributes.
+
+    Returns:
+        dict: A dictionary containing information about the game's scope.
+
+    Raises:
+        FileNotFoundError: If the specified JSON file is not found.
+        json.JSONDecodeError: If there is an issue decoding the JSON data.
+    """
     try:
         with open("data/game_scope_data.json", "r") as file:
             return json.load(file)
